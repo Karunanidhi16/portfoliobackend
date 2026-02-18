ac1b3a2bbe76e8fdbffd3e11d800b9ab29abbe24
Karunanidhi Sharma <Karunanidhisharma16@users.noreply.github.com>
2026-02-19 00:03:02 +0530

    Update CORS configuration to allow requests from the production frontend domain

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Allow requests from your frontend domain
 app.use(cors({
-  origin: ['https://karunanidhii16.vercel.app', 'http://localhost:5173'],
+  origin: ['https://portfolio.logicryx.in', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
